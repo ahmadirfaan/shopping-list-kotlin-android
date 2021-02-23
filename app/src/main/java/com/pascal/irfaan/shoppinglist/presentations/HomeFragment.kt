44f9e0ee32c86238983837856b55cb52acc42814
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2021-02-23 13:03:58 +0700

    Add Binding

--- app/src/main/java/com/pascal/irfaan/shoppinglist/presentations/HomeFragment.kt
+++ app/src/main/java/com/pascal/irfaan/shoppinglist/presentations/HomeFragment.kt
@@ -4,23 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.pascal.irfaan.shoppinglist.R
-import com.pascal.irfaan.shoppinglist.models.Item
-import com.pascal.irfaan.shoppinglist.utils.ItemListConfig
+import com.pascal.irfaan.shoppinglist.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
-    private lateinit var goToAddItemButton : Button
-    private lateinit var goToListButton : Button
-    private lateinit var goToAccountButton : Button
-    private lateinit var navController : NavController
-    private lateinit var itemList : MutableList<Item>
+    lateinit var binding: FragmentHomeBinding
+    private lateinit var navController: NavController
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,28 +25,27 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-        itemList = ItemListConfig
-        goToAddItemButton = view.findViewById(R.id.goToAddItem)
-        goToListButton = view.findViewById(R.id.goToList)
-        goToAccountButton = view.findViewById(R.id.goToAccount)
-        return view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
+        binding.apply {
+            goToAddItem.setOnClickListener {
+                navController.navigate(R.id.action_homeFragment_to_addItem)
+            }
+            goToList.setOnClickListener {
+                navController.navigate(R.id.action_homeFragment_to_viewListShopping)
+            }
+            goToAccount.setOnClickListener {
+                navController.navigate(R.id.action_homeFragment_to_accountFragment)
+            }
+        }
 
-        val bundle = bundleOf("ITEM_LIST" to itemList)
-        goToAddItemButton.setOnClickListener {
-            navController.navigate(R.id.action_homeFragment_to_addItem)
-        }
-        goToListButton.setOnClickListener {
-            navController.navigate(R.id.action_homeFragment_to_viewListShopping, bundle)
-        }
-        goToAccountButton.setOnClickListener {
-            navController.navigate(R.id.action_homeFragment_to_accountFragment)
-        }
+
     }
 
     companion object {
--- app/src/main/java/com/pascal/irfaan/shoppinglist/presentations/MainActivity.kt
+++ app/src/main/java/com/pascal/irfaan/shoppinglist/presentations/MainActivity.kt
@@ -6,16 +6,19 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.pascal.irfaan.shoppinglist.R
+import com.pascal.irfaan.shoppinglist.databinding.ActivityMainBinding
 import com.pascal.irfaan.shoppinglist.models.Item
 import com.pascal.irfaan.shoppinglist.utils.OnNavigationListener
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
 }
