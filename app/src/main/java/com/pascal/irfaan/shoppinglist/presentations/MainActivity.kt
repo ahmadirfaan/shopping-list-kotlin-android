672d7f4b7e07f949589762709c5c734fbd0b0546
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2021-02-25 21:41:42 +0700

    Add Bottom Navigation View

--- app/src/main/java/com/pascal/irfaan/shoppinglist/presentations/MainActivity.kt
+++ app/src/main/java/com/pascal/irfaan/shoppinglist/presentations/MainActivity.kt
@@ -2,17 +2,33 @@ package com.pascal.irfaan.shoppinglist.presentations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.pascal.irfaan.shoppinglist.R
 import com.pascal.irfaan.shoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        binding.apply {
+            bottomNavigationView.setupWithNavController(navController)
+        }
     }
 
 }
